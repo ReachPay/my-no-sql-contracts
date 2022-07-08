eb5f7cb2f319cfa36fe3cc42e4af00dc9c9e6ee2
amigin <amigin@gmail.com>
2022-07-08 14:22:18 +0300

    Added asset_entity

---
+++ src/asset_entity.rs
@@ -0,0 +1,39 @@
+use my_no_sql_server_abstractions::MyNoSqlEntity;
+use rust_extensions::date_time::DateTimeAsMicroseconds;
+use serde::*;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BidAskMyNoSqlEntity {
+    #[serde(rename = "PartitionKey")]
+    pub partition_key: String,
+    #[serde(rename = "RowKey")]
+    pub row_key: String,
+    #[serde(rename = "TimeStamp")]
+    pub time_stamp: String,
+}
+
+impl BidAskMyNoSqlEntity {
+    pub fn generate_partition_key() -> &str {
+        "assets"
+    }
+
+    pub fn get_asset_id(&self) -> &str {
+        &self.row_key
+    }
+}
+
+impl MyNoSqlEntity for BidAskMyNoSqlEntity {
+    fn get_partition_key(&self) -> &str {
+        &self.partition_key
+    }
+
+    fn get_row_key(&self) -> &str {
+        &self.row_key
+    }
+
+    fn get_time_stamp(&self) -> i64 {
+        DateTimeAsMicroseconds::parse_iso_string(self.time_stamp.as_str())
+            .unwrap()
+            .unix_microseconds
+    }
+}
