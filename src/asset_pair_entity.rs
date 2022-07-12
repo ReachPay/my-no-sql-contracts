4b61e41d17a39f3d3f150eaa2884a141e41ddad9
amigin <amigin@gmail.com>
2022-07-12 20:45:25 +0300

    Added Assets and AssetsPairs and ConvertPairs

--- src/asset_pair_entity.rs
+++ src/asset_pair_entity.rs
@@ -2,31 +2,35 @@ use my_no_sql_server_abstractions::MyNoSqlEntity;
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 use serde::*;
 
+pub const ASSET_PAIR_TABLE_NAME: &str = "asset-pairs";
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct AssetEntity {
+pub struct AssetPairEntity {
     #[serde(rename = "PartitionKey")]
     pub partition_key: String,
     #[serde(rename = "RowKey")]
     pub row_key: String,
     #[serde(rename = "TimeStamp")]
     pub time_stamp: String,
-    #[serde(rename = "Accuracy")]
-    pub accuracy: usize,
+    #[serde(rename = "FromSymbol")]
+    pub from_symbol: String,
+    #[serde(rename = "ToSymbol")]
+    pub to_symbol: String,
     #[serde(rename = "IsEnabled")]
     pub is_enabled: bool,
 }
 
-impl AssetEntity {
+impl AssetPairEntity {
     pub fn generate_partition_key() -> &'static str {
-        "assets"
+        "asset-pairs"
     }
 
-    pub fn get_asset_id(&self) -> &str {
+    pub fn get_asset_pair_id(&self) -> &str {
         &self.row_key
     }
 }
 
-impl MyNoSqlEntity for AssetEntity {
+impl MyNoSqlEntity for AssetPairEntity {
     fn get_partition_key(&self) -> &str {
         &self.partition_key
     }
