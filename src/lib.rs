4493dc4a392cd447d084e94284ccd1c463287493
Hays R <wonderhays@gmail.com>
2022-11-28 18:05:25 +0200

    added confirm codes

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,5 @@ pub mod price_bridge_config;
 pub mod price_bridge_instrument_mapping_entity;
 #[cfg(feature = "price-bridge-settings-entity")]
 pub mod price_bridge_settings_entity;
+#[cfg(feature = "confirm_2fa_codes")]
+pub mod confirm_2fa_codes;
