f9c70f7249e9b4ac6c8938a387dfe0e2d2dfd96f
amigin <amigin@gmail.com>
2022-12-16 22:23:45 +0200

    Plugged Macros

--- src/asset_pair_entity.rs
+++ src/asset_pair_entity.rs
@@ -1,17 +1,10 @@
-use my_no_sql_server_abstractions::MyNoSqlEntity;
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 use serde::*;
 
 pub const ASSET_PAIR_TABLE_NAME: &str = "asset-pairs";
-
+#[my_no_sql_macros::my_no_sql_entity]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AssetPairEntity {
-    #[serde(rename = "PartitionKey")]
-    pub partition_key: String,
-    #[serde(rename = "RowKey")]
-    pub row_key: String,
-    #[serde(rename = "TimeStamp")]
-    pub time_stamp: String,
     #[serde(rename = "FromSymbol")]
     pub from_symbol: String,
     #[serde(rename = "ToSymbol")]
@@ -25,21 +18,5 @@ impl AssetPairEntity {
 
     pub fn get_asset_pair_id(&self) -> &str {
         &self.row_key
-    }
-}
-
-impl MyNoSqlEntity for AssetPairEntity {
-    fn get_partition_key(&self) -> &str {
-        &self.partition_key
-    }
-
-    fn get_row_key(&self) -> &str {
-        &self.row_key
-    }
-
-    fn get_time_stamp(&self) -> i64 {
-        DateTimeAsMicroseconds::parse_iso_string(self.time_stamp.as_str())
-            .unwrap()
-            .unix_microseconds
     }
 }
--- src/bid_ask_entity.rs
+++ src/bid_ask_entity.rs
@@ -1,19 +1,11 @@
-use my_no_sql_server_abstractions::MyNoSqlEntity;
 use rust_extensions::date_time::DateTimeAsMicroseconds;
 use serde::*;
 
 pub const BID_ASK_SNAPSHOT_TABLE_NAME: &str = "bid-ask-snapshot";
 pub const PARTITION_KEY: &str = "bidask";
-
+#[my_no_sql_macros::my_no_sql_entity]
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct BidAskMyNoSqlEntity {
-    #[serde(rename = "PartitionKey")]
-    pub partition_key: String,
-    #[serde(rename = "RowKey")]
-    pub row_key: String,
-    #[serde(rename = "TimeStamp")]
-    pub time_stamp: String,
-
     #[serde(rename = "Bid")]
     pub bid: f64,
     #[serde(rename = "Ask")]
@@ -37,21 +29,5 @@ impl BidAskMyNoSqlEntity {
 
     pub fn get_date_time(&self) -> DateTimeAsMicroseconds {
         DateTimeAsMicroseconds::parse_iso_string(self.date_time.as_str()).unwrap()
-    }
-}
-
-impl MyNoSqlEntity for BidAskMyNoSqlEntity {
-    fn get_partition_key(&self) -> &str {
-        &self.partition_key
-    }
-
-    fn get_row_key(&self) -> &str {
-        &self.row_key
-    }
-
-    fn get_time_stamp(&self) -> i64 {
-        DateTimeAsMicroseconds::parse_iso_string(self.time_stamp.as_str())
-            .unwrap()
-            .unix_microseconds
     }
 }
